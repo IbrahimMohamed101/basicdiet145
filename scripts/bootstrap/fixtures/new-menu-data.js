790f1ae581191044d6b88d3ec80ee526ffcd461e
Ibrahim <108367693+IbrahimMohamed101@users.noreply.github.com>
2026-07-15 23:42:09 +0000

    refactor(menu): compose new menu fixture modules

--- scripts/bootstrap/fixtures/new-menu-data.js
+++ scripts/bootstrap/fixtures/new-menu-data.js
@@ -1,6 +1,13 @@
+const categories = require("./new-menu-categories");
+const mainCourses = require("./new-menu-main_courses");
+const breakfast = require("./new-menu-breakfast");
+const sandwiches = require("./new-menu-sandwiches");
+const salads = require("./new-menu-salads");
+const optionGroups = require("./new-menu-options");
+
 module.exports = {
-  "source": "Basic Diet new menu workbook",
-  "categories": [],
-  "products": [],
-  "optionGroups": []
+  source: "Basic Diet new menu workbook",
+  categories,
+  products: [...mainCourses, ...breakfast, ...sandwiches, ...salads],
+  optionGroups,
 };
