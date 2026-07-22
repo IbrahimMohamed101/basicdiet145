2f8cdd4b53bc4eccfe71859234c0c1f335800f12
Ibrahim <108367693+IbrahimMohamed101@users.noreply.github.com>
2026-07-22 08:22:17 +0000

    test(addons): remove temporary reconciliation diagnostics

--- tests/runSubscriptionDailyAddonPolicyWithReservation.test.js
+++ tests/runSubscriptionDailyAddonPolicyWithReservation.test.js
@@ -6,26 +6,4 @@ require("../src/services/installSubscriptionDailyAddonPolicy");
 require("../src/services/installSubscriptionAddonReservationClosure");
 require("../src/services/installSubscriptionAddonReservationReconciliation");
 require("../src/services/installSubscriptionAddonOpsIdentityClosure");
-
-const dailyAddonService = require("../src/services/subscription/subscriptionDailyAddonService");
-const composedEnsure = dailyAddonService.ensureDailyAddonDefaultsForDay;
-dailyAddonService.ensureDailyAddonDefaultsForDay = async function diagnosticEnsure(args = {}) {
-  const result = await composedEnsure(args);
-  const firstSelection = result && result.day && Array.isArray(result.day.addonSelections)
-    ? result.day.addonSelections[0]
-    : null;
-  console.log("[addons][reconciliation-diagnostic]", JSON.stringify({
-    normalizationUpdatedCount: result && result.normalizationUpdatedCount,
-    firstSelection: firstSelection && {
-      name: firstSelection.name,
-      nameI18n: firstSelection.nameI18n,
-      autoDailyAddon: firstSelection.autoDailyAddon,
-      dailyAllocationKey: firstSelection.dailyAllocationKey,
-      subscriptionAddonLabelI18n: firstSelection.subscriptionAddonLabelI18n,
-      resolvedProductNameI18n: firstSelection.resolvedProductNameI18n,
-    },
-  }));
-  return result;
-};
-
 require("./subscriptionDailyAddonPolicy.integration.test");
