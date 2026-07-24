09626e27e8001ef45ed078c89ec9919ff06c15c0
Ibrahim <108367693+IbrahimMohamed101@users.noreply.github.com>
2026-07-24 16:40:24 +0000

    test: isolate database helper with memory mongo

--- tests/helpers/dbHelper.js
+++ tests/helpers/dbHelper.js
@@ -1,11 +1,27 @@
 const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 const { logger } = require("../../src/utils/logger");
 
 const { resolveMongoUri } = require("../../src/utils/mongoUriResolver");
 
+let ownedMemoryServer = null;
+let previousTestMongoUri;
+
+async function ensureIsolatedTestMongoUri() {
+  if (process.env.NODE_ENV !== "test" || process.env.MONGO_URI_TEST) return;
+
+  const dbName = `basicdiet_helper_${process.pid}_${Date.now()}_test`;
+  ownedMemoryServer = await MongoMemoryServer.create({
+    instance: { dbName },
+  });
+  previousTestMongoUri = process.env.MONGO_URI_TEST;
+  process.env.MONGO_URI_TEST = ownedMemoryServer.getUri(dbName);
+}
+
 async function connectDB() {
   if (mongoose.connection.readyState === 0) {
     try {
+      await ensureIsolatedTestMongoUri();
       const uri = resolveMongoUri();
       await mongoose.connect(uri, {
         serverSelectionTimeoutMS: 10000,
@@ -21,6 +37,13 @@ async function connectDB() {
 async function disconnectDB() {
   if (mongoose.connection.readyState !== 0) {
     await mongoose.disconnect();
+  }
+  if (ownedMemoryServer) {
+    await ownedMemoryServer.stop();
+    ownedMemoryServer = null;
+    if (previousTestMongoUri === undefined) delete process.env.MONGO_URI_TEST;
+    else process.env.MONGO_URI_TEST = previousTestMongoUri;
+    previousTestMongoUri = undefined;
   }
 }
 
