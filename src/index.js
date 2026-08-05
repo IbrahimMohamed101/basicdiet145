e3c865630d4a9eaa32eaf826b51906e4711045a7
Ibrahim <108367693+IbrahimMohamed101@users.noreply.github.com>
2026-08-05 22:57:46 +0000

    feat(stacking): install default-closed shadow observer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("./services/installSubscriptionStackingShadowProjection");
 require("./services/installUpcomingSubscriptionPlanningBalance");
 require("./services/installOneTimeOrderItemTypeCompatibility");
 
