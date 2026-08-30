3cb9fb3d8015cec5920a02403d1216a96878f85a
Ibrahim <108367693+IbrahimMohamed101@users.noreply.github.com>
2026-08-30 18:26:31 +0300

    Expose superadmin subscription financial controls

--- src/routes/dashboardCustomerManagement.js
+++ src/routes/dashboardCustomerManagement.js
@@ -2,6 +2,7 @@
 
 const { Router } = require("express");
 const controller = require("../controllers/dashboardCustomerManagementController");
+const financialControlController = require("../controllers/dashboardSubscriptionFinancialControlController");
 const asyncHandler = require("../middleware/asyncHandler");
 const {
   dashboardAuthMiddleware,
@@ -14,6 +15,14 @@ router.use(dashboardAuthMiddleware, dashboardRoleMiddleware(["superadmin"]));
 router.get("/:id", asyncHandler(controller.getCustomer));
 router.patch("/:id", asyncHandler(controller.updateCustomer));
 router.post("/:id/meal-compensations", asyncHandler(controller.grantMealCompensation));
+router.get(
+  "/:id/subscriptions/:subscriptionId/financial-control",
+  asyncHandler(financialControlController.preview)
+);
+router.post(
+  "/:id/subscriptions/:subscriptionId/financial-control",
+  asyncHandler(financialControlController.execute)
+);
 router.post("/:id/account-merge/preview", asyncHandler(controller.previewAccountMerge));
 router.post("/:id/account-merge", asyncHandler(controller.mergeAccounts));
 
